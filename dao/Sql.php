7d8c6417f92f49d8ef90c03ea26942170ceadb36
Felipe Lamim <fclamim@gmail.com>
2019-02-06 11:58:33 -0200

    finalizando primeiro DAO

--- dao/Sql.php
+++ dao/Sql.php
@@ -19,8 +19,19 @@ class SQL extends PDO{
 
     public function query($rawQuery, $params =  array()){
         $stmt = $this->conn->prepare($rawQuery);
+        $this->setParams($stmt, $params);
 
+       $stmt->execute();
+
+       return $stmt;
         
+    }
+
+    public function select($rawQuery, $params = array()):array{
+      $stmt = $this->query($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     }
 }
 
